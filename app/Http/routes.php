0941183baf3d3e3fc555cafa56e4e14b9b65889f
Raine Alvarez <Raine Alvarez>
2017-01-01 19:04:31 +0800

    Added file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,4 +22,14 @@ Route::group(['middleware' => ['web']], function() {
         'uses' => 'UserController@postSignUp',
         'as' => 'signup'
     ]);
+
+    Route::post('/signin', [
+        'uses' => 'UserController@postSignIn',
+        'as' => 'signin'
+    ]);
+    Route::get('/dashboard', [
+        'uses' => 'UserController@getDashboard',
+        'as' => 'dashboard'
+
+    ]);
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
         </div>
         <div class="col-md-6">
             <h3>Sign In
-                <form action="#" method="post">
+                <form action="{{ route('signin') }}" method="post">
                     <div class="form-group">
                         <label for="email">Your E-Mail</label>
                         <input class="form-control" type="text" name="email" id="email">
@@ -37,6 +37,7 @@
                         <input class="form-control" type="password" name="password" id="password">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
+                    <input type="hidden" name="_token" value="{{ Session::token() }}">
 
                 </form>
         </div>
