43e6d8e6bdb297a74c4e2de4d545c30f8253adda
Raine Alvarez <Raine Alvarez>
2017-01-01 18:31:41 +0800

    New Files Added!

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,19 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+/*...*/
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +13,13 @@ Route::get('/', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
+Route::group(['middleware' => ['web']], function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::post('/signup', [
+        'uses' => 'UserController@postSignUp',
+        'as' => 'signup'
+    ]);
 });
