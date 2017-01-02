d1159e4dbc611395f71a2c073d9fafe4708aacae
Raine Alvarez <Raine Alvarez>
2017-01-02 19:43:43 +0800

    Modified Filesgit add -A

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,11 +27,7 @@ Route::group(['middleware' => ['web']], function() {
         'uses' => 'UserController@postSignIn',
         'as' => 'signin'
     ]);
-    Route::get('/dashboard', [
-        'uses' => 'PostController@getDashboard',
-        'as' => 'dashboard',
-        'middleware' => 'auth'
-    ]);
+
     Route::post('/createpost', [
         'uses' => 'PostController@postCreatePost',
         'as' => 'post.create',
@@ -54,6 +50,16 @@ Route::group(['middleware' => ['web']], function() {
         'uses' => 'UserController@postSaveAccount',
         'as' => 'account.save'
     ]);
+    Route::get('/userimage/{filename}', [
+        'uses' => 'UserController@getUserImage',
+        'as' => 'account.image'
+    ]);
+    Route::get('/dashboard', [
+        'uses' => 'PostController@getDashboard',
+        'as' => 'dashboard',
+        'middleware' => 'auth'
+    ]);
+
     Route::post('/edit', [
         'uses' => 'PostController@postEditPost',
         'as' => 'edit'
