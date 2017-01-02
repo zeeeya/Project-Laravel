b3537132a17d4f9c09240f1bc7d9a31ab4daaa96
Raine Alvarez <Raine Alvarez>
2017-01-02 18:43:05 +0800

    Modified Files

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,9 @@
 
 
-$('.post').find('.interaction').find('a').eq(2).on('click', function() {
+$('.post').find('.interaction').find('.edit').on('click', function (event) {
+    event.preventDefault();
+
+    var postBody = event.target.parentNode.parentNode.childNodes[1].textContent;
+    $('#post-body').val(postBody);
     $('#edit-modal').modal();
 });
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,7 +28,7 @@
         <a href="#">Dislike</a>
         @if(Auth::user() == $post->user)
             |
-            <a href="#">Edit</a> |
+            <a href="#" class="edit">Edit</a> |
             <a href="{{ route('post.delete', ['post_id' => $post->id]) }}">Delete</a>
         @endif
     </div>
