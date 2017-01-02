92ff0a28c0441414e5becc7560bd0504bd703ecf
Raine Alvarez <Raine Alvarez>
2017-01-02 18:21:49 +0800

    Added and Modified Files

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,7 +28,7 @@
         <a href="#">Dislike</a>
         @if(Auth::user() == $post->user)
             |
-            <a href="#" id="post-edit" data-postid="{{ $post->id }}">Edit</a> |
+            <a href="#">Edit</a> |
             <a href="{{ route('post.delete', ['post_id' => $post->id]) }}">Delete</a>
         @endif
     </div>
@@ -36,4 +36,26 @@
                 @endforeach
         </div>
     </section>
+    <div class="modal fade" tabindex="-1" role="dialog" id="edit-modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Edit Post</h4>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="post-body">Edit the Post</label>
+                            <textarea class="form-control" name="post-body" id="post-body" rows="5"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" id="modal-save">Save changes</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 @endsection
